b8b6869ec9a8fa47bc78bf9481f62ba08e0c9866
CarlosDS369 <carlos@delfos369.com>
2022-03-03 19:32:59 -0600

    Update am.js

--- controllers/am.js
+++ controllers/am.js
@@ -1,24 +1,24 @@
 const pool = require( '../src/db');
+const{response} = require('express');
+const am = {};
 
-exports.insertAm = async(req,res)=>{
-    const insertAm = req.body;
-   await pool.query("INSERT INTO am set ?",insertAm);
-   
-   res.json({
-           msg:"se agregado correctamente am",
-           estado:true
-   })
-}
-
-exports.updateAm = async(req,res)=>{
-    const {id}= req.params; 
-    const updateAm = req.body;
-   await pool.query("UPDATE am set ? WHERE id_am=?",[updateAm,id]);
-   
-   res.json({
-           msg:"se modifico  am",
-           estado:true
-   })
+//Función para agregar atributos en la tabla am---------------------------------------------------
+am.insert_am = async (req,res) =>{
+        const {new_id_proyecto} = req.params; 
+        const new_am = { 
+                am_valor_dolar,
+                am_desc_cliente,
+                am_margen_ganancia,
+                am_desc_fabrica,
+                am_id_proyecto: new_id_proyecto
+        } = req.body;
+        //new_am.am_valor_dolar = 21.22; //Dato prueba para inserción
+        //new_am.am_id_proyecto = 1; //Dato prueba para inserción
+        await pool.query('INSERT INTO am SET ?', [new_am]);
+        res.json({
+                msg: "AM agregado exitosamente",
+                estado: true,
+                });
 }
 //------------------------------------------------------------------------------------------------
 
