61ddb3ee98b2351816799c8a6a1f342e2e179713
CarlosDS369 <carlos@delfos369.com>
2022-03-14 09:01:00 -0600

    Update servicios_productos.js

--- controllers/servicios_productos.js
+++ controllers/servicios_productos.js
@@ -1,23 +1,31 @@
 const pool = require("../src/db");
 const sp = {};
 
-// Función para agregar atributos en la tabla servicio_producto
+// Función para agregar atributos en la tabla servicio_producto y psp
 sp.insert_sp = async (req, res) => {
-  // const { new_sp_id_precio, new_sp_id_proveedor, new_sp_id_categoria } = req.params;
+  const {id} = req.params;
   const new_sp= {
     sp_no_parte,
     sp_descripcion,
     sp_meses,
     sp_semanas,
     sp_cantidad, 
-    // sp_id_precio: new_sp_id_precio,
-    // sp_id_proveedor: new_sp_id_proveedor,
-    // sp_id_categoria: new_sp_id_categoria,
+    sp_id_precio,
+    sp_id_proveedor,
+    sp_id_categoria,
     sp_comentarios,
   } = req.body;
-  console.log(req.body)
-  //new_sp.sp_no_parte = 50; //Dato para prueba
-  await pool.query('insert into servicio_producto set ?', [new_sp]);
+  //console.log(req.body)
+  //new_sp.sp_no_parte = 70; //Dato para prueba
+  const reSql = await pool.query('insert into servicio_producto set ?', [new_sp]);
+
+  const psp ={
+    psp_id_partida:id,
+    psp_id_sp:reSql.insertId
+  }
+  console.log()
+  const reSql2 = await pool.query("INSERT INTO psp set ?", [psp]);
+  /* DEVUELVE RESPUESTA AL FRONT LOS SIGUIENTES DATOS*/
   res.json({
     msg: "Producto agregado exitosamente",
     estado: true,
