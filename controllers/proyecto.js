65af164cae449926c7d5ed117cb815e9459c2b4e
CarlosDS369 <carlos@delfos369.com>
2022-03-17 12:50:28 -0600

    Update proyecto.js

--- controllers/proyecto.js
+++ controllers/proyecto.js
@@ -63,26 +63,3 @@ exports.updateProyectos = async(req,res)=>{
     console.log(reSql);
   };
 
-  // Función para consultar los atributos de un determinado proyecto
-  exports.viewSerchProyecto = async (req, res) => {
-    const {proyecto_id} = req.params;
-    const reSql = await pool.query(
-      "SELECT partida_id,partida_nombre,partida_descripcion,"
-      +"sp_no_parte, sp_descripcion, sp_meses, sp_semanas, sp_cantidad,"
-      +"precio_lista, precio_unitario, precio_descuento, precio_total, moneda_nombre,"
-      +"proveedor_nombre, marca_nombre, categoria_nombre, sp_comentarios "
-      +"FROM proyecto "
-      +"LEFT JOIN pp ON pp_id_proyecto = proyecto_id "
-      +"LEFT JOIN partida ON pp_id_partida = partida_id "
-      +"LEFT JOIN psp ON psp_id_partida = partida_id "
-      +"LEFT JOIN servicio_producto ON psp_id_sp = sp_id "
-      +"LEFT JOIN precio ON sp_id_precio = precio_id "
-      +"LEFT JOIN moneda ON precio_id_moneda = moneda_id "
-      +"LEFT JOIN proveedor ON sp_id_proveedor = proveedor_id "
-      +"LEFT JOIN proveedor_marca ON pm_id_proveedor = proveedor_id "
-      +"LEFT JOIN marca ON pm_id_marca = marca_id "
-      +"LEFT JOIN categoria ON sp_id_categoria = categoria_id "
-      +"WHERE proyecto_id = ?", [proyecto_id]);
-    res.json({data:reSql});
-    console.log(reSql);
-  };
