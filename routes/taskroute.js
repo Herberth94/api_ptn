da72ab88b550c3c4cd4303dee3bdf808682b0cf0
AndradeMalaika <malaikaandrade20@gmail.com>
2022-04-05 19:53:24 -0500

    arreglando error de resetear contraseña

--- routes/taskroute.js
+++ routes/taskroute.js
@@ -40,7 +40,7 @@ module.exports= function(){
         // ruta para editar los parametros en base Id
         router.post('/edit/:id', editForm);  
         // ruta para editar el password en base Id
-        router.put('/edit/pass/:id', editPass);         
+        router.post('/edit/pass/:id', editPass);         
 
 /*--------------------------------------------------------------------------------------------------------------*/
 /*=====TABLA DE PARTIDA ========*/
