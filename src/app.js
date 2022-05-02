f42c49cff84d37946e5f9c217fd90e74e5b8e9ee
oscar <oscar.delfos369.com>
2022-05-02 01:52:02 -0500

    change

--- src/app.js
+++ src/app.js
@@ -1,3 +1,5 @@
+
+
 const express =  require('express');
 const cors = require('cors');
 const morgan = require('morgan');
@@ -7,22 +9,26 @@ const app = express();
 
 //Middleware 
 app.use(cors());
+const corsOptions ={
+
+  origin:'http://localhost:3000',
+/*   origin:'http://oscarcm23.github.io/demo-final/',  
+    */
+   
+  credentials:true,            //access-control-allow-credentials:true
+  optionSuccessStatus:200
+}
 //  para poder recibir respuest json 
 app.use(express.json({extend:true}));
     // Ver el tipo de peticion y la ruta
 app.use(morgan('dev'));
+
 //router
   //app.use('/api/cotizador',require('../routes/taskroute'));
-  app.use('/api/cotizador',routes());
-  app.use((err, req, res, next) => {
-    console.error(err.stack)
-    res.status(400).json({
-      name: err.name
-
-
-    })
-  })
+  app.use('/api/cotizador', cors(corsOptions) ,routes());
  //   app.use('/',)
 
 //exportando app
 module.exports = app ;
+
+
