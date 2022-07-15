1063dc0461c423c12e345fcaf81f788ab7b2131d
Cotizador <herberth@delfos369.com>
2022-07-15 21:51:06 +0200

    cambio de puerto

--- src/index.js
+++ src/index.js
@@ -2,8 +2,8 @@ const app =  require('./app');
 const pool = require('./db');
 require ('dotenv').config();
 //Escuchar peticiones al puerto
-app.listen( process.env.PORT || 4000 , () => {
-    console.log('Servidor corriendo en el puerto: ' , process.env.PORT);
+app.listen( 4001 , () => {
+    console.log('Servidor corriendo en el puerto: ' ,4001);
 })
 //conexión a la base de Datos
 
